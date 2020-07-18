765688b219d91d88e10e3fff72459ece65a5c393
Ada Rose Cannon <ada@ada.is>
2020-07-18 15:53:58 +0100

    don't keep opening the file

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ extern crate rscam;
 
 use framebuffer::{Framebuffer};
 use std::fs::File;
-use std::io::Write;
+use std::io::{Write, Seek, SeekFrom};
 
 fn main () {
 
     let mut camera = rscam::new("/dev/video0").unwrap();
     let framebuffer = Framebuffer::new("/dev/fb1").unwrap();
+    let mut file = File::create("/dev/fb1").expect("Unable to open");
 
     let w = framebuffer.var_screen_info.xres;
     let h = framebuffer.var_screen_info.yres;
@@ -22,7 +23,7 @@ fn main () {
     }
 
     camera.start(&rscam::Config {
-        interval: (1, 20),      // 10 fps.
+        interval: (1, 30),
         resolution: (w, h),
         format: b"RGB3",
         ..Default::default()
@@ -30,8 +31,8 @@ fn main () {
 
     let mut buf:Vec<u16> = Vec::with_capacity(length);
     loop {
-        let mut file = File::create("/dev/fb1").expect("Unable to open");
         let frame = camera.capture().unwrap();
+        file.seek(SeekFrom::Start(0)).expect("can't reset file pointer");
 
         buf.clear();
         buf.extend(frame.chunks_exact(3).map(|px| {
@@ -45,5 +46,6 @@ fn main () {
         };
 
         file.write_all(&u8_slice).expect("Unable to write");
+        // framebuffer.write_frame(&u8_slice);
     }
 }
