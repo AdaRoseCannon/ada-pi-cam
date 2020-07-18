5fc97c49203a9f6498353eea91d965cb0fdea088
Ada Rose Cannon <ada@ada.is>
2020-07-18 18:37:42 +0100

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,82 @@
 extern crate framebuffer;
 extern crate rscam;
+extern crate rppal;
+extern crate rust_embed;
 
 use framebuffer::{Framebuffer};
 use std::fs::File;
 use std::io::{Write, Seek, SeekFrom};
+use rppal::gpio::Gpio;
+use rust_embed::RustEmbed;
+
+
+#[derive(RustEmbed)]
+#[folder = "assets/"]
+struct Asset;
 
 fn main () {
 
-    let mut camera = rscam::new("/dev/video0").unwrap();
-    let framebuffer = Framebuffer::new("/dev/fb1").unwrap();
-    let mut file = File::create("/dev/fb1").expect("Unable to open");
-
+    let screensaver = Asset::get("cam.bmp").unwrap();
+    let mut camera: rscam::Camera = rscam::new("/dev/video0").unwrap();
+    let framebuffer: framebuffer::Framebuffer  = Framebuffer::new("/dev/fb1").unwrap();
+    let mut file: std::fs::File = File::create("/dev/fb1").expect("Unable to open");
     let w = framebuffer.var_screen_info.xres;
     let h = framebuffer.var_screen_info.yres;
     let length:usize = (w * h) as usize;
+    let mut buf:Vec<u16> = Vec::with_capacity(length);
+
+    file.write_all(&screensaver[138..]).expect("Can't write to framebuffer");
+
+    let mut mode:u8 = 0; // 0 idle, 1 taking photo, 2, showing preview
+
     println!("Resolution {} {}", w, h);
 
-    for wformat in camera.formats() {
-        let format = wformat.unwrap();
-        println!("{:?}", format);
-        println!("  {:?}", camera.resolutions(&format.format).unwrap());
-    }
+    let gpio = Gpio::new().expect("Could not open GPIO");
+    let gpio17 = gpio.get(27).expect("Could not open GPIO 17");
+    let gpio17input = gpio17.into_input();
+    let mut gpio17input_prev:bool = false;
 
-    camera.start(&rscam::Config {
-        interval: (1, 30),
-        resolution: (w, h),
-        format: b"RGB3",
-        ..Default::default()
-    }).unwrap();
-
-    let mut buf:Vec<u16> = Vec::with_capacity(length);
     loop {
-        let frame = camera.capture().unwrap();
-        file.seek(SeekFrom::Start(0)).expect("can't reset file pointer");
+        match mode {
+            0 => {
+                let val = gpio17input.is_high();
+                if val != gpio17input_prev {
+                    println!("GPIO 17 is high? {}", val);
+                    match val {
+                        false => {}
+                        true => {
+                            mode = 1;
+                            camera.start(&rscam::Config {
+                                interval: (1, 30),
+                                resolution: (w, h),
+                                format: b"RGB3",
+                                ..Default::default()
+                            }).unwrap();
+                        }
+                    }
+                }
+                gpio17input_prev = val;
+            }   
+            1 => {
 
-        buf.clear();
-        buf.extend(frame.chunks_exact(3).map(|px| {
-            ((px[0] as u16) >> 3) << 11 |
-            ((px[1] as u16) >> 2) << 5 |
-            ((px[2] as u16) >> 3)
-        }));
-    
-        let u8_slice: &[u8] = unsafe {
-            std::slice::from_raw_parts(buf.as_ptr().cast(), length*2)
-        };
+                file.seek(SeekFrom::Start(0)).expect("Can't reset file pointer");
 
-        file.write_all(&u8_slice).expect("Unable to write");
-        // framebuffer.write_frame(&u8_slice);
+                let frame = camera.capture().unwrap();
+            
+                buf.clear();
+                buf.extend(frame.chunks_exact(3).map(|px| {
+                    ((px[0] as u16) >> 3) << 11 |
+                    ((px[1] as u16) >> 2) << 5 |
+                    ((px[2] as u16) >> 3)
+                }));
+            
+                let u8_slice: &[u8] = unsafe {
+                    std::slice::from_raw_parts(buf.as_ptr().cast(), buf.len()*2)
+                };
+            
+                file.write_all(&u8_slice).expect("Unable to write");
+            }
+            _=>{}
+        }
     }
 }
