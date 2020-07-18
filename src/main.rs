af22855260345e5857965bcefe4bc2a4054f1fea
Ada Rose Cannon <ada@ada.is>
2020-07-18 13:10:47 +0100

    Kornel's perf improvements

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ use std::fs::File;
 use std::io::Write;
 
 fn main () {
-    let mut framebuffer = Framebuffer::new("/dev/fb1").unwrap();
+
+    let mut camera = rscam::new("/dev/video0").unwrap();
+    let framebuffer = Framebuffer::new("/dev/fb1").unwrap();
 
     let w = framebuffer.var_screen_info.xres;
     let h = framebuffer.var_screen_info.yres;
-    let length = w * h;
+    let length:usize = (w * h) as usize;
     println!("Resolution {} {}", w, h);
-
-    let mut camera = rscam::new("/dev/video0").unwrap();
 
     for wformat in camera.formats() {
         let format = wformat.unwrap();
@@ -22,26 +22,28 @@ fn main () {
     }
 
     camera.start(&rscam::Config {
-        interval: (1, 10),      // 10 fps.
+        interval: (1, 20),      // 10 fps.
         resolution: (w, h),
         format: b"RGB3",
         ..Default::default()
     }).unwrap();
 
-    let mut u8_buffer: Vec<u8> = Vec::with_capacity((length * 2) as usize);
-
-    for i in 0..300 {
-        u8_buffer.clear();
-        let frame = camera.capture().unwrap();
+    let mut buf:Vec<u16> = Vec::with_capacity(length);
+    loop {
         let mut file = File::create("/dev/fb1").expect("Unable to open");
+        let frame = camera.capture().unwrap();
 
-        for x in 0..length {
-            let red =   frame[(x*3 + 0) as usize];
-            let green = frame[(x*3 + 1) as usize];
-            let blue =  frame[(x*3 + 2) as usize];
-            u8_buffer.push((green >> 2 << 5) + (blue >> 3));
-            u8_buffer.push((red >> 3 << 3) + (green >> 5));
-        }
-        file.write_all(&u8_buffer).expect("Unable to write");
+        buf.clear();
+        buf.extend(frame.chunks_exact(3).map(|px| {
+            ((px[0] as u16) >> 3) << 11 |
+            ((px[1] as u16) >> 2) << 5 |
+            ((px[2] as u16) >> 3)
+        }));
+    
+        let u8_slice: &[u8] = unsafe {
+            std::slice::from_raw_parts(buf.as_ptr().cast(), length*2)
+        };
+
+        file.write_all(&u8_slice).expect("Unable to write");
     }
 }
